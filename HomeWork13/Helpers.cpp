b5d35f6860b2b7cac58892ae1f40bcbbecf4210e
Artur <arturrabbit3@gmail.com>
2020-05-26 00:32:32 +0300

    Добавьте файлы проекта.

---
+++ HomeWork13/Helpers.cpp
@@ -0,0 +1,7 @@
+#include "Helpers.h"
+
+int GetSquareOfSum(int arg1, int arg2)
+{
+	auto sum = arg1 + arg2;
+	return sum * sum;
+}
---
+++ HomeWork13/Helpers.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int GetSquareOfSum(int arg1, int arg2);
